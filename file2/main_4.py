4b51bfe0720bee54dd0b8e99c641ad684a947882
Corsaro-Elena <corsaro.elena@iisgalvanimi.edu.it>
2023-03-17 10:41:01 +0000

    V1

--- file2/main_4.py
+++
@@ -1,9 +0,0 @@
-#mi collego a mydatbase
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="mydatabase"
-)
